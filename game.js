7dedd2131fea8a08c7315479bfe176cc38929c7c
Justin McGuire <lifefeed+github@gmail.com>
2010-12-30 12:39:41 -0500

    refactored unit setup into setup_unit(), less repeated code

--- game.js
+++ game.js
@@ -16,107 +16,79 @@ $.fn.xinc_board = function(options, placements) {
         var action_allowed = player_i == opts.this_player ? 1 : 0;
         
         $.each(player.armies, function(player_id, piece) {
-            var i = piece.at[0];
-            var j = piece.at[1];
-
-            var cell = $("#cell_" + i + "-" + j);
-            draw_thing(cell, "army " + player.color);
-
-            if (action_allowed) {
-                var select_actions = get_select_actions(cell);
-                var move_actions = get_move_actions(cell);
-                /* every selectable cell gets two functions attached to it, one if it is selected,
-                   and another if it is deselected, which cleans up. */
-                cell.data('select', function(){
-                    select_actions.select();
-                    move_actions.select();
-                });
-                cell.data('deselect', function(){
-                    select_actions.deselect();
-                    move_actions.deselect();
-                });
-
-                cell.click(function(){
-                    if ($(this).hasClass("selected")) {
-                        $(this).data('deselect')();
-                    } else {
-                        $('.selected').each(function(){ $(this).data('deselect')() });
-                        $(this).data('select')();
-                    }
-                });
-            }
+            setup_army(piece, player.color, action_allowed);
         });
         
         $.each(player.settlers, function(player_id, piece) {
-            var i = piece.at[0];
-            var j = piece.at[1];
-
-            var cell = $("#cell_" + i + "-" + j);
-            draw_thing(cell, "settler " + player.color);
-
-            if (action_allowed) {
-                var select_actions = get_select_actions(cell);
-                var move_actions = get_move_actions(cell);
-                var settle_actions = get_settle_actions(cell);
-                
-                /* every selectable cell gets two functions attached to it, one if it is selected,
-                   and another if it is deselected, which cleans up. */
-                cell.data('select', function(){
-                    select_actions.select();
-                    move_actions.select();
-                    settle_actions.select();
-                });
-                cell.data('deselect', function(){
-                    select_actions.deselect();
-                    move_actions.deselect();
-                    settle_actions.deselect();
-                });
-                
-                cell.click(function(){
-                    if ($(this).hasClass("selected")) {
-                        $(this).data('deselect')();
-                    } else {
-                        $('.selected').each(function(){ $(this).data('deselect')() });
-                        $(this).data('select')();
-                    }
-                });
-            }
+            setup_settler(piece, player.color, action_allowed);
         });
         
         $.each(player.cities, function(player_id, piece) {
-            var i = piece.at[0];
-            var j = piece.at[1];
-            
-            var cell = $("#cell_" + i + "-" + j);
-            draw_thing(cell, "city " + player.color);
-            
-            if (0) { /* check if under construction */
-                ; /* no actions are allowed */
-            } else if (action_allowed) {
-                var select_actions = get_select_actions(cell);
-                var build_actions = get_build_actions(cell);
-                
-                /* every selectable cell gets two functions attached to it, one if it is selected,
-                   and another if it is deselected, which cleans up. */
-                cell.data('select', function(){
-                    select_actions.select();
-                    build_actions.select();
-                });
-                cell.data('deselect', function(){
-                    select_actions.deselect();
-                    build_actions.deselect();
-                });
-                
-                cell.click(function(){
-                    if ($(this).hasClass("selected")) {
-                        $(this).data('deselect')();
-                    } else {
-                        $('.selected').each(function(){ $(this).data('deselect')() });
-                        $(this).data('select')();
-                    }
-                });
-            }
+            setup_city(piece, player.color, action_allowed);
         });
+    }
+
+    /*
+     *
+     * Setup each unit, for this player and otherwise.
+     *   piece          - object, from the init, looks like { at: [3,2], status: "" }
+     *   player_color   - string, matches up to a color defined in CSS
+     *   action_allowed - boolean
+     *   unit_type      - string, "settler", "army", or "city". matched up to a unit type in CSS.
+     *   action_fns     - array of functions, of the get_*_actions from below
+     */
+     
+    /* some shortcuts to the setup_unit function */
+    function setup_army (piece, player_color, action_allowed) {
+        setup_unit(piece, player.color, action_allowed, "army", [get_select_actions, get_move_actions] );
+    }
+    function setup_settler (piece, player_color, action_allowed) {
+        setup_unit(piece, player.color, action_allowed, "settler", [get_select_actions, get_move_actions, get_settle_actions] );
+    }
+    function setup_city (piece, player_color, action_allowed) {
+        setup_unit(piece, player.color, action_allowed, "city", [get_select_actions, get_build_actions] );
+    }
+    
+    function setup_unit (piece, player_color, action_allowed, unit_type, action_fns) {
+        var i = piece.at[0];
+        var j = piece.at[1];
+
+        var cell = $("#cell_" + i + "-" + j);
+        draw_thing(cell, unit_type + ' ' + player_color);
+
+        if (action_allowed) {
+            var select_fns = [];
+            var deselect_fns = [];
+            
+            for (i in action_fns) {
+                var actions = action_fns[i](cell);
+                select_fns.push(actions.select);
+                deselect_fns.push(actions.deselect);
+            }
+            
+            /* every selectable cell gets two functions attached to it, one if it is selected,
+               and another if it is deselected, which cleans up. */
+            /*alert($.dump(select_fns));*/
+            cell.data('select', function(){
+                for (i in select_fns) {
+                    select_fns[i]();
+                }
+            });
+            cell.data('deselect', function(){
+                for (i in deselect_fns) {
+                    deselect_fns[i]();
+                }
+            });
+            
+            cell.click(function(){
+                if ($(this).hasClass("selected")) {
+                    $(this).data('deselect')();
+                } else {
+                    $('.selected').each(function(){ $(this).data('deselect')() });
+                    $(this).data('select')();
+                }
+            });
+        }
     }
 
     /* 
@@ -146,11 +118,11 @@ $.fn.xinc_board = function(options, placements) {
     }
 
     /* 
-     * Our actions for each tile
+     * Our action sets for each unit.
      *
      */
 
-    /* make the cell "glow", or some other look-at-me effect */
+    /* make the unit "glow", or some other look-at-me effect */
     function get_select_actions(cell) {
         var select_fn = function(){
             cell.addClass("selected");
@@ -178,43 +150,32 @@ $.fn.xinc_board = function(options, placements) {
         return { select: select_fn, deselect: deselect_fn };
     }
 
+    /* settle actions let a unit create a city */
     function get_settle_actions(cell) {
-        /* show the "city" icon, make it selectable */
-        var img = $('<img src="images/transparent.png" class="mini-icon mini-city" alt="build a city" title="build a city" \>');
+        var img_city = $('<img src="images/transparent.png" class="mini-icon mini-city" alt="build a city" title="build a city" \>');
 
         var select_fn = function(){
-            cell.append(img);
-
-            /* place the image within the cell */
-            var cell_offset = cell.offset();
-            var top = cell_offset.top + opts.cell_size - opts.min_icon_margin - img.height();
-            var left = cell_offset.left + opts.cell_size / 2 - img.width() / 2;
-            img.offset({ top: top, left: left });
+            cell.append(img_city);
+            place_image_bottom_center(cell, img_city);
         }
 
         var deselect_fn = function(){
-            img.remove();
+            img_city.remove();
         }
 
         return { select: select_fn, deselect: deselect_fn };
     }
 
+    /* build actions let a unit create an army or a settler */
     function get_build_actions(cell) {
-        /* show the "army" and "settler" icons */
-        var img_settler = $('<img src="images/transparent.png" class="mini-icon mini-settler" alt="build a settler" title="build a city" \>');
-        var img_army = $('<img src="images/transparent.png" class="mini-icon mini-army" alt="build an army" title="build a city" \>');
+        var img_settler = $('<img src="images/transparent.png" class="mini-icon mini-settler" alt="build a settler" title="build a settler" \>');
+        var img_army = $('<img src="images/transparent.png" class="mini-icon mini-army" alt="build an army" title="build an army" \>');
 
         var select_fn = function(){
             cell.append(img_army);
             cell.append(img_settler);
-
-            /* place the images within the cell */
-            var cell_offset = cell.offset();
-            var top = cell_offset.top + opts.cell_size - opts.min_icon_margin - img_army.height();
-            var left1 = cell_offset.left + opts.min_icon_margin;
-            var left2 = cell_offset.left + opts.cell_size - img_army.width() - opts.min_icon_margin;
-            img_army.offset({ top: top, left: left1 });
-            img_settler.offset({ top: top, left: left2 });
+            place_image_bottom_right(cell, img_settler);
+            place_image_bottom_left(cell, img_army);
         }
 
         var deselect_fn = function(){
@@ -225,6 +186,35 @@ $.fn.xinc_board = function(options, placements) {
         return { select: select_fn, deselect: deselect_fn };
     }
 
+    /*
+     * Place a mini-icon within the cell.
+     *
+     * These functions take in two jquery objects, a <td> cell, and an <img>. The img must have
+     * already been placed (be visible in the cell, like append()'ed).
+     *
+     * Each function will move the img to the appropriate place within the cell.
+     *
+     * Note: There's no guarantee that mini-icons won't overlap.
+     * Note: These functions take opts.min_icon_margin into account.
+     */
+    function place_image_bottom_left(cell, img) {
+        var cell_offset = cell.offset();
+        var top = cell_offset.top + opts.cell_size - opts.min_icon_margin - img.height();
+        var left = cell_offset.left + opts.min_icon_margin;
+        img.offset({ top: top, left: left });
+    }
+    function place_image_bottom_right(cell, img) {
+        var cell_offset = cell.offset();
+        var top = cell_offset.top + opts.cell_size - opts.min_icon_margin - img.height();
+        var left = cell_offset.left + opts.cell_size - img.width() - opts.min_icon_margin;
+        img.offset({ top: top, left: left });
+    }
+    function place_image_bottom_center(cell, img) {
+        var cell_offset = cell.offset();
+        var top = cell_offset.top + opts.cell_size - opts.min_icon_margin - img.height();
+        var left = cell_offset.left + opts.cell_size / 2 - img.width() / 2;
+        img.offset({ top: top, left: left });
+    }
 };
 
 $.fn.xinc_board.opts_defaults = {
